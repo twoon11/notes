f84a02713980bdfed9393c69d0ad00548c4066fe
twoon11 <132555546+twoon11@users.noreply.github.com>
2023-05-17 23:09:35 +1000

    Update uiner-grep-v2.sh

--- uiner/uiner-grep-v2.sh
+++ uiner/uiner-grep-v2.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env sh
 
-outputDir="/mnt/c/Users/voznyukva/uin"			# Полный или относительный путь до Каталога с выходными данными вида UUID.txt
-dataFile="/mnt/c/Users/voznyukva/data.json"		# Полный или относительный путь и имя файла с входными данными в json
-taskFile="/mnt/c/Users/voznyukva/tasks.txt"		# Полный или относительный путь и имя файла с UUID-ами. Одна строка -- один UUID
+outputDir="/mnt/c/Users/uin"			# Полный или относительный путь до Каталога с выходными данными вида UUID.txt
+dataFile="/mnt/c/Users/data.json"		# Полный или относительный путь и имя файла с входными данными в json
+taskFile="/mnt/c/Users/tasks.txt"		# Полный или относительный путь и имя файла с UUID-ами. Одна строка -- один UUID
 
 maxProcs=$(grep -i 'core id' /proc/cpuinfo|wc -l)
 mkdir -p $outputDir
